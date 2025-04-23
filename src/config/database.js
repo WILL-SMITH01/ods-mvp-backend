bea5edc1712dea156a1c05fdfd26a1f9ca00d5e3
Marlon Bruno <your@example.com>
2025-04-23 17:31:31 -0300

    alterando porta

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || 'app_user',
   password: process.env.DB_PASSWORD || 'app_pass123',
   database: process.env.DB_NAME || 'mvp_ods_db',
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 3307,
 });
 
 export default pool;
